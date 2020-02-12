25fade3df2166f1cd90a89b09ad6fcfe053a3653
Teodora321 <teodoraakehaiovaa@gmail.com>
2020-02-12 13:25:02 +0200

    added components

--- e-commerce/src/app/app-routing.module.ts
+++ e-commerce/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: 'register',
+    component:RegisterComponent
+  },
+  {
+    path: "**",
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- e-commerce/src/app/core/core.module.ts
+++ e-commerce/src/app/core/core.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, FooterComponent],
+  declarations: [NavigationComponent, FooterComponent, LoginComponent, RegisterComponent, NotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  exports: [NavigationComponent,FooterComponent]
+  exports: [NavigationComponent,FooterComponent, LoginComponent, RegisterComponent,NotFoundComponent]
 })
 export class CoreModule { }
