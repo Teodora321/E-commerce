c799312af998cc738000fc36fc24d3a2c335c404
Teodora321 <teodoraakehaiovaa@gmail.com>
2020-02-18 00:37:09 +0200

    cart

--- e-commerce/src/app/products/product-cart/product-cart.component.ts
+++ e-commerce/src/app/products/product-cart/product-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { IProduct } from 'src/app/shared/interfaces/product';
 
@@ -8,19 +8,22 @@ import { IProduct } from 'src/app/shared/interfaces/product';
   styleUrls: ['./product-cart.component.css']
 })
 export class ProductCartComponent implements OnInit {
+
+  //@ViewChild('qtyInput', { static: false }) qtyInput: ElementRef<HTMLInputElement>;
   
   get currentUser() {
     return this.userService.currentUser;
   }
-  products:any;
+  products:IProduct[];
   constructor(private userService:UserService) { }
 
   ngOnInit() {
     this.userService.getCartItems().subscribe((data) => {
-      this.products= Object.values(data)
-      
+      this.products = Object.values(data);
+      console.log(Object.values(data))
     })
   }
+  deleteCart(){}
 
 
 }
